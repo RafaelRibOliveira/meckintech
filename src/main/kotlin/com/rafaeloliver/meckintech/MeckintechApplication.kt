1844784c541a5a8e0579cd0fb32a4adb662c5e87
Rafael OLiveira <rafaeloliver789@gmail.com>
2020-09-28 17:20:35 -0300

    Projeto criado

---
+++ src/main/kotlin/com/rafaeloliver/meckintech/MeckintechApplication.kt
@@ -0,0 +1,11 @@
+package com.rafaeloliver.meckintech
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MeckintechApplication
+
+fun main(args: Array<String>) {
+	runApplication<MeckintechApplication>(*args)
+}
